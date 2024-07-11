6d323aabc7265018064d9b894977cffe03fb6323
86187 <86187@DESKTOP-DIUS65T>
2024-07-11 23:54:15 +0800

    添加项目文件。

---
+++ FIT5032_MyCodeSnippet/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using FIT5032_MyCodeSnippet.Exercise;
+using FIT5032_MyCodeSnippet.HelloWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FIT5032_MyCodeSnippet.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            Hello hello = new Hello();
+            // I assigned the ViewBag.Message to the result
+            ViewBag.Message = hello.GetHello();
+            ExampleDictionary ed = new ExampleDictionary();
+            ed.Example();
+
+            return View();
+           
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
